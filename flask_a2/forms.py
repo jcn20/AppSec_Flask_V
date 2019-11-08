82f2cfc419e8db72608e7271b2e30213860f9082
Julio Nunez <jcn327@nyu.edu>
2019-11-08 00:27:18 -0500

    Added ids to each variable in forms.py.

--- flask_a2/forms.py
+++ flask_a2/forms.py
@@ -6,9 +6,9 @@ from flask_a2.models import User
 
 
 class RegistrationForm(FlaskForm):
-    uname = StringField('Username', validators=[DataRequired(), Length(min=5, max=20)])
+    uname = StringField('Username', id='uname',  validators=[DataRequired(), Length(min=5, max=20)])
     mfa = PasswordField('2fa', id="2fa", validators=[DataRequired(), Length(min=10, max=11)])
-    pword = PasswordField('Password', validators=[DataRequired()])
+    pword = PasswordField('Password', id='pword', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
 
     def validate_uname(self, uname):
@@ -25,9 +25,9 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    uname = StringField('Username', validators=[DataRequired(), Length(min=5, max=20)])
+    uname = StringField('Username', id='uname', validators=[DataRequired(), Length(min=5, max=20)])
     mfa = PasswordField('2fa', id="2fa", validators=[DataRequired(), Length(min=10, max=11)])
-    pword = PasswordField('Password', validators=[DataRequired()])
+    pword = PasswordField('Password', id='pword',  validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
